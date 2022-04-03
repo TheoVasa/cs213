fddfc162cc19f4935bb79111e7186c374b0d76b9
vasarino <theo.vasarino@epfl.ch>
2022-04-03 16:27:35 +0200

    physics engine(3.2) - THEO

---
+++ Assets/Scripts/CollisionsExample.cs
@@ -0,0 +1,38 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CollisionsExample : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    void OnCollisionEnter(Collision collisionInfo)
+    {
+        print("Detected collsion between " + gameObject.name + " and " + collisionInfo.collider.name);
+        print("There are " + collisionInfo.contacts.Length + " point(s) of contacts");
+        print("Their relative velocity is " + collisionInfo.relativeVelocity);
+
+    }
+
+    void OnCollisionStay(Collision collisionInfo)
+    {
+        print(gameObject.name + " and " + collisionInfo.collider.name + " are still colliding");
+
+    }
+
+    void OnCollisionExit(Collision collisionInfo)
+    {
+        print(gameObject.name + " and " + collisionInfo.collider.name + " are no longer colliding");
+
+    }
+}
